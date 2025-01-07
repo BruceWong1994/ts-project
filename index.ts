f61fcbafd632335605d942cee3bfb584b4d1ddb5
767961652@qq.com <767961652@qq.com>
2025-01-07 16:32:39 +0800

    submit to another laptop

--- index.ts
+++ index.ts
@@ -3,3 +3,9 @@ const person = {
     age: 18
 }
 console.log(`我叫${person.name},我今年${person.age}岁`);
+
+// let arr:number[] = ['a','545']
+let a1:number[] = [165,328,5]
+let a2:[string,number]
+// a2 = ['af']
+a2 = ['af',11]
